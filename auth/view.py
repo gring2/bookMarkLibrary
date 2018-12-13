d28e78312a9bd02afa952713a728bb39dd80ca17
gring2 <pjw6454@hotmail.com>
2018-12-13 12:41:09 +0900

    login feature

--- auth/view.py
+++ auth/view.py
@@ -1,11 +1,18 @@
-from bookMarkLibrary import user_datastore, db
+from bookMarkLibrary import user_datastore, db, User
 from . import bp
 from flask import (current_app as app, render_template, abort, request, redirect, url_for)
-
+from flask_security import login_user
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         user_datastore.create_user(email=request.form['email'], password=request.form['password'])
         db.session.commit()
+        return redirect('/')
+
+@bp.route('login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        user = User(**request.form)
+        login_user(user)
         return redirect('/')
--- tests/tests_auth/test_http.py
+++ tests/tests_auth/test_http.py
@@ -3,7 +3,7 @@ from flask import url_for, current_app as app
 from flask_migrate import downgrade, Config
 from bookMarkLibrary import User, db
 from tests.base import BaseTestCase
-from flask_security import login_user, current_user
+from flask_security import login_user, current_user, AnonymousUser
 import unittest
 
 class AuthTest(BaseTestCase):
@@ -17,9 +17,19 @@ class AuthTest(BaseTestCase):
         self.assertEqual(current_user, user, current_user.id)
         self.assertEqual(current_user.email, user.email, current_user.email)        
         db.drop_all(bind=None)
-    @unittest.skip
+
     def test_login(self):
-        self.fail('login user')
+        zero = User.query.count()
+        self.assertEqual(0,zero)
+        self.client.post(url_for('auth.login'), data={'email':'test@test.com', 'password':'test123'})
+        self.assertTrue(current_user.is_anonymous)
+        self.client.post(url_for('auth.register'), data={'email':'test@test.com', 'password':'test123'})
+
+        cnt = User.query.count()
+        self.assertEqual(1,cnt)
+
+        self.client.post(url_for('auth.login'), data={'email':'test@test.com', 'password':'test123'})
+        self.assertIsNotNone(current_user)
 
     def tearDown(self):
         super().tearDown()
