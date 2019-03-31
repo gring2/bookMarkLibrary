e6390c07801f56689272927472430428aa28f5d9
gring2 <pjw6454@hotmail.com>
2019-03-31 14:55:33 +0900

    skip selenium tests

--- tests/tests_library/test_http.py
+++ tests/tests_library/test_http.py
@@ -32,7 +32,7 @@ class AddTestCase(BaseTestCase):
         os.makedirs(app.config['STORAGE_PATH'])
         super().tearDown()
 
-#@skip
+@skip
 class ShowTestCase(BaseTestCase):
 
     def setUp(self):
