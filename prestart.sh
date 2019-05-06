2f991bf3f94541750a69d8aa97a8e4107b06ae0e
gring2 <pjw6454@hotmail.com>
2019-05-06 19:05:16 +0900

    gcsfuse test_1

--- prestart.sh
+++ prestart.sh
@@ -4,3 +4,10 @@
 sleep 10;
 # Run migrations
 FLASK_APP=$PWD/bookMarkLibrary/run.py flask db upgrade
+
+if [ ! -z "$STORAGE_PATH" ] ; \
+  then gcsfuse -o allow_other $STORAGE_PATH /app/bookMarkLibrary/storage ; \
+  fi
+if [ ! -z "$LOG_PATH" ] ; \
+  then gcsfuse -o allow_other $LOG_PATH /app/log ; \
+  fi
